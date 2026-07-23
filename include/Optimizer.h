f6d16d92115e339035de0e352b9765b80ea2019f
Beginner10617 <wasihusain23@iitk.ac.in>
2026-07-23 16:23:34 +0530

    Changing optimizer, working on control flow graphs

--- include/Optimizer.h
+++ include/Optimizer.h
@@ -13,10 +13,10 @@ class CFG {
 public:
   explicit CFG(Function &func);
   BasicBlock *entry() const;
-  std::vector<BasicBlock *> reverse_postorder() const;
+  std::vector<BasicBlock> reverse_postorder() const;
 
 private:
-  std::vector<BasicBlock *> blocks_;
+  std::vector<BasicBlock> blocks_;
 };
 
 class Optimizer {
@@ -27,10 +27,5 @@ public:
 
 private:
   Compiler *compiler;
-  std::unordered_map<size_t, big_int> const_vars;
-  std::set<std::string> funcalls;
-  std::optional<big_int> get_const(const Arg &arg);
-  void remove_const(const Var &arg);
-  void constant_folding(Func &func);
-  struct Const_Folding_Visitor;
+  std::map<std::string, CFG> func_control_flows;
 };
